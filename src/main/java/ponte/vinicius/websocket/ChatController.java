5ddd405cc1315c7824622fa8dd974d03c002ce08
vinicius <vinicius.mponte@gmail.com>
2026-02-02 21:49:03 -0300

    feat: Primeiro commit, chat simples com WebSocket

---
+++ src/main/java/ponte/vinicius/websocket/ChatController.java
@@ -0,0 +1,15 @@
+package ponte.vinicius.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message) {
+        return message;
+    }
+}
